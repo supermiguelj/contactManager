4987bb6afc084be2f5ede92a0d89ec71174b92af
supermiguelj <chevymj2001@gmail.com>
2025-02-16 05:56:38 -0500

    Finished the API for the search contact function

---
+++ LAMPAPI/SearchContact.php
@@ -0,0 +1,48 @@
+<?php
+    // Gets POST request from searchContact() function in code.js
+    $inData = getRequestInfo();
+    
+    // Connects to MariaDB
+    $database = new mysqli("localhost", "Test", "Dummy", "Manager");
+    
+    if ( $database->connect_error ) { // Invalid connection
+		die(json_encode(["success" => false, "message" => $database->connect_error]));
+	} else { // Connection Established
+        // Cleans the search (% allows for incomplete words)
+        $name = "%" . $database->real_escape_string($inData->name) . "%";
+        // Prepares query to search for contact by name
+        $prepStmt = $database->prepare("SELECT * FROM Contacts WHERE name LIKE ?");
+        $prepStmt->bind_param("s", $name);
+        $prepStmt->execute();
+
+        $contacts = [];
+        // Stores result for retrieval
+        $result = $prepStmt->get_result();
+
+        // Packages result into JSON to deliver back to searchContact() in front end
+        while($row = $result->fetch_assoc()) {
+			$contacts[] = $row;
+		}
+
+        $prepStmt->close();
+        $database->close();
+
+        // Return JSON response
+        if (count($contacts) > 0) {
+            echo json_encode(["success" => true, "contacts" => $contacts]);
+        } else {
+            echo json_encode(["success" => false, "message" => "No contacts found."]);
+        }
+    }
+
+    // Used to unpack and retrieve raw JSON output
+    function getRequestInfo() {
+		return json_decode(file_get_contents('php://input'));
+	}
+
+    // Sends back the results as JSON
+	function sendResultInfoAsJson($obj) {
+		header('Content-type: application/json');
+		echo $obj;
+	}
+?>
